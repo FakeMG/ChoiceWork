c3d20a46bf2ab7afac838e56ee6710e3aca2dc37
hieuvuhv34 <hieuvuhv34@gmail.com>
2022-11-27 17:41:03 +0700

    change layout of schedule fragment

--- app/src/main/java/com/example/decisions/MainActivity.java
+++ app/src/main/java/com/example/decisions/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.decisions;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
@@ -22,5 +25,14 @@ public class MainActivity extends AppCompatActivity {
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
         NavController navController = navHostFragment.getNavController();
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        replaceFragment(new Schedule());
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_schedule, fragment);
+        fragmentTransaction.commit();
     }
 }
