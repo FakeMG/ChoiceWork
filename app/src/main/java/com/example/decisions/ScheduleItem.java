b056608a2e0f69542bba835faa658692875c794c
hieuvuhv34 <hieuvuhv34@gmail.com>
2022-11-27 17:39:53 +0700

    create schedule item of board

---
+++ app/src/main/java/com/example/decisions/ScheduleItem.java
@@ -0,0 +1,18 @@
+package com.example.decisions;
+
+public class ScheduleItem {
+
+    private int resourceImage;
+
+    public ScheduleItem(int resourceImage) {
+        this.resourceImage = resourceImage;
+    }
+
+    public int getResourceImage() {
+        return resourceImage;
+    }
+
+    public void setResourceImage(int resourceImage) {
+        this.resourceImage = resourceImage;
+    }
+}
