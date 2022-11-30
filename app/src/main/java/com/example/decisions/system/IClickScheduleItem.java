23f045987db9dba66649c1032691a53803ea7dca
hieuvuhv34 <hieuvuhv34@gmail.com>
2022-12-01 00:40:00 +0700

    add interface for click schedule item

---
+++ app/src/main/java/com/example/decisions/system/IClickScheduleItem.java
@@ -0,0 +1,7 @@
+package com.example.decisions.system;
+
+import com.example.decisions.ScheduleItem;
+
+public interface IClickScheduleItem {
+    void onClickScheduleItem(ScheduleItem scheduleItem);
+}
