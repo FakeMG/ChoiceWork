0a4c43e680d95f78678694d70845fdd50f336a1c
hieuvuhv34 <hieuvuhv34@gmail.com>
2022-12-02 01:22:18 +0700

    put data to schedule board activity

--- app/src/main/java/com/example/decisions/controller/ScheduleFragmentController.java
+++ app/src/main/java/com/example/decisions/controller/ScheduleFragmentController.java
@@ -1,32 +1,38 @@
 package com.example.decisions.controller;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.decisions.R;
 import com.example.decisions.model.ScheduleBoardModel;
+import com.example.decisions.view.activity.ScheduleBoardActivity;
 
 import java.util.ArrayList;
 
 public class ScheduleFragmentController {
     private Context context;
     private ArrayList<ScheduleBoardModel> listSchedule;
-    private Bundle bundleScheduleItem;
 
     public ScheduleFragmentController(Context context) {
         this.context = context;
         this.listSchedule = new ArrayList<>();
-        this.bundleScheduleItem = new Bundle();
         dataInitialize();
     }
 
     public void onClickGoToBoard(ScheduleBoardModel scheduleBoardModel) {
-        bundleScheduleItem.putSerializable("schedule_board", scheduleBoardModel);
+        Bundle bundleScheduleBoard = new Bundle();
+        bundleScheduleBoard.putSerializable("schedule_board", scheduleBoardModel);
+        Intent intent = new Intent(getContext(), ScheduleBoardActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("schedule_board", scheduleBoardModel);
+        intent.putExtras(bundle);
+        getContext().startActivity(intent);
     }
 
     private ArrayList<ScheduleBoardModel> dataInitialize() {
         for (int i = 1; i <= 13; i++) {
-            listSchedule.add(new ScheduleBoardModel("Schedule " + i, R.drawable.ic_launcher_background));
+            listSchedule.add(new ScheduleBoardModel("Schedule " + i, R.drawable.learning_01));
         }
 
         return listSchedule;
@@ -46,13 +52,5 @@ public class ScheduleFragmentController {
 
     public void setListSchedule(ArrayList<ScheduleBoardModel> listSchedule) {
         this.listSchedule = listSchedule;
-    }
-
-    public Bundle getBundleScheduleItem() {
-        return bundleScheduleItem;
-    }
-
-    public void setBundleScheduleItem(Bundle bundleScheduleItem) {
-        this.bundleScheduleItem = bundleScheduleItem;
     }
 }
