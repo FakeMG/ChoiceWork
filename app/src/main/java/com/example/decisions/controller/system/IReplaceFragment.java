9175f72354c2b573fe92bf6099aead66fc19d7e0
hieuvuhv34 <hieuvuhv34@gmail.com>
2022-12-01 22:36:51 +0700

    add interface replace fragment

---
+++ app/src/main/java/com/example/decisions/controller/system/IReplaceFragment.java
@@ -0,0 +1,7 @@
+package com.example.decisions.controller.system;
+
+import androidx.fragment.app.Fragment;
+
+public interface IReplaceFragment {
+    void replaceFragment(int fragmentId, Fragment fragment);
+}
