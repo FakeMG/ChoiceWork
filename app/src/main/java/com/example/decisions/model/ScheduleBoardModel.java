1e66c8a968d20b23f2d52250ac5863b957bdb091
hieuvuhv34 <hieuvuhv34@gmail.com>
2022-12-02 16:18:20 +0700

    change layout for schedule activity

--- app/src/main/java/com/example/decisions/model/ScheduleBoardModel.java
+++ app/src/main/java/com/example/decisions/model/ScheduleBoardModel.java
@@ -2,29 +2,9 @@ package com.example.decisions.model;
 
 import java.io.Serializable;
 
-public class ScheduleBoardModel implements Serializable {
-
-    private String name;
-    private int resourceImage;
+public class ScheduleBoardModel extends ImageModel implements Serializable {
 
     public ScheduleBoardModel(String name, int resourceImage) {
-        this.name = name;
-        this.resourceImage = resourceImage;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getResourceImage() {
-        return resourceImage;
-    }
-
-    public void setResourceImage(int resourceImage) {
-        this.resourceImage = resourceImage;
+        super(name, resourceImage);
     }
 }
