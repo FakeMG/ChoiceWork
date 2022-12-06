5600d9f6bfcae0d162f812bc779ef9fa3e954db2
Vũ Thanh Lâm <79557871+FakeMG@users.noreply.github.com>
2022-12-06 16:47:38 +0700

    feat: add toolbar interaction with scrolling

--- app/src/main/java/com/example/decisions/view/activity/MainActivity.java
+++ app/src/main/java/com/example/decisions/view/activity/MainActivity.java
@@ -1,18 +1,14 @@
 package com.example.decisions.view.activity;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
-import android.os.Bundle;
-
 import com.example.decisions.R;
-import com.example.decisions.controller.system.IReplaceFragment;
-import com.example.decisions.view.fragment.schedule.ScheduleFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,10 +18,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         // link bottom navigation and fragments
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
         NavController navController = navHostFragment.getNavController();
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
     }
 }
