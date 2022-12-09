38ba0086789c8cdd44964bc9dff902015ad16f3a
Hieubentau <79564920+Hieubentau@users.noreply.github.com>
2022-12-09 15:29:40 +0700

    add WaitingBoardActivity

--- app/src/main/java/com/example/decisions/controller/system/IClickScheduleBoard.java
+++ app/src/main/java/com/example/decisions/controller/system/IClickScheduleBoard.java
@@ -1,6 +1,6 @@
 package com.example.decisions.controller.system;
 
-import com.example.decisions.model.ScheduleBoardModel;
+import com.example.decisions.model.schedule.ScheduleBoardModel;
 
 public interface IClickScheduleBoard {
     void onClickScheduleBoard(ScheduleBoardModel scheduleBoardModel);
---
+++ app/src/main/java/com/example/decisions/view/activity/WaitingBoardActivity.java
@@ -0,0 +1,16 @@
+package com.example.decisions.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.decisions.R;
+
+public class WaitingBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waiting_board);
+    }
+}
